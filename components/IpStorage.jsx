248539071b2d180200540f2efec295d3b726ddbe
satyam pathak <satyampathak67@gmail.com>
2024-09-29 10:41:14 +0530

    ground breaking changes

--- components/IpStorage.jsx
+++ components/IpStorage.jsx
@@ -2,26 +2,12 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getUsername } from './Storage';
 import { router } from 'expo-router';
 import axios from 'axios';
-import testProxy from '../utils/testproxy';
-import Tor from 'react-native-tor';
-import check from '../utils/testOnion';
-let DEFAULT_IP_ADDRESS = 'http://16.171.206.219:80';
+// let DEFAULT_IP_ADDRESS = 'http://16.171.206.219:80';
 const ORBOT_HTTP_PROXY = 'http://127.0.0.1:8118'; 
-const onionAddress = 'http://hwbl6cafsrwrcb4ulrsgst3jzfaf22a222744ovygty3ugkpnimygfid.onion/';
+const onionAddress = 'http://hwbl6cafsrwrcb4ulrsgst3jzfaf22a222744ovygty3ugkpnimygfid.onion';
 export const saveIPAddress = async (ipAddress) => {
   try {
-    const onionAddress = 'http://check.torproject.org/';
-    const val=await testProxy()
-    console.log(val)
     const usr = await getUsername();
-    response =await check()
-
-console.log(response)
-    
-
-
-
-
     await AsyncStorage.setItem('ipAddress', ipAddress);
     console.log('IP address saved successfully');
   } catch (error) {
@@ -30,18 +16,26 @@ console.log(response)
     
   }
 };
-
 export const getIPAddress = async () => {
   try {
     let ipAddress = await AsyncStorage.getItem('ipAddress');
-    if (ipAddress !== null) {
-      return ipAddress; 
-    } else {
-      return DEFAULT_IP_ADDRESS;
-    }
+    ipAddress=ipAddress?ipAddress:onionAddress
+    const usr = await getUsername();
+    const res=await axios.post(`${ipAddress}/users`,{username:usr})
+    console.log(res)
+    return ipAddress;
   } catch (error) {
-    console.error('Error retrieving IP address:', error);
-    return DEFAULT_IP_ADDRESS;
+    if (error.response) {
+      if (error.response.status === 400) {
+        console.log('working: Status 400 caught in catch block');
+      } else {
+        console.error(`Request failed with status ${error.response.status}`);
+      }
+    } else {
+      console.error('Network error or request could not be made:', error.message);
+    }
+
+    return onionAddress;
   }
 };
 
